f56249659f65e8e02cb1b99fbce4aa6b1c3fd700
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2024-03-11 20:14:41 +0200

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>Investor Directives Administrator</title>
+          <meta
+            property="og:title"
+            content="Investor Directives Administrator"
+          />
+        </Head>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
